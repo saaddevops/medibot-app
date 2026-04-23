4de2b1d5314d9c4d551d48f564c435600eb45179
saaddevops <67101229+saaddevops@users.noreply.github.com>
2026-04-23 15:24:08 +0530

    Update MedibotApplication.java

--- src/main/java/com/medibot/MedibotApplication.java
+++ src/main/java/com/medibot/MedibotApplication.java
@@ -1,1 +1,11 @@
+package com.medibot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedibotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MedibotApplication.class, args);
+    }
+}
